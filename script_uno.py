b703d9d12dde11e87930ce10fea3b743d49a29a1
as22star <168441682+as22star@users.noreply.github.com>
2024-04-30 21:32:35 -0500

    Create script_uno.py

---
+++ script_uno.py
@@ -0,0 +1,9 @@
+print("¡Hola, mundo!")
+
+print("Explicacion de GIT & GITHUB")
+
+print("Grupo 8")
+
+print("Kevin Mantilla, Estefania Bedon, Andres Salvatore, Diego Pomboza, Alejandro Guerrero")
+
+print(Hola mundo)
